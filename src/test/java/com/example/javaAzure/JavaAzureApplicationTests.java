a14b8a9464b779712f50004e51c071147bc09511
Priyarani26 <37960381+Priyarani26@users.noreply.github.com>
2023-03-08 01:30:14 +0530

    first commit

---
+++ src/test/java/com/example/javaAzure/JavaAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaAzure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
